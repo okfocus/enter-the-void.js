21809fe9f60124801dda3e97e1f63c722a47eac2
Jonathan Vingiano <jvingiano@gmail.com>
2012-04-16 15:02:00 -0400

    add scroll

--- src/oktype.js
+++ src/oktype.js
@@ -46,10 +46,12 @@
 
         base.start = function () {
 
+            if (base.options.on != 'hover' || base.options.on != 'scroll') base.hoverIn(base.$el); // Animate in perpetuity
+
+            // If the function .on() is available to us (jQuery > 1.7), use it. Otherwise fallback to .bind().
+            if (typeof $().on === 'function') {
             // Should the animation execute on hover?
-            if (base.options.hover) {
-                // If the function .on() is available to us (jQuery > 1.7), use it. Otherwise fallback to .bind().
-                if(typeof $().on === "function"){
+                if (base.options.on === 'hover') {
                     base.$el.on({
                         // On hover
                         mouseenter: function(){
@@ -60,7 +62,18 @@
                             base.hoverOut($(this));
                         }
                     });
+                } else if (base.options.on === 'scroll') {
+                    $(window).on({
+                        // On scroll
+                        scroll: function(e){
+                            base.hoverIn($(base.options.elArray.join()), e.type);
+                        }
+                    });
                 } else {
+                    console.log("OKType Error: The 'on' option must be set to 'hover' or 'scroll'.");
+                }                
+            } else {
+                if (base.options.on === 'hover') {
                     base.$el.bind({
                         // On hover
                         mouseenter: function(){
@@ -71,10 +84,16 @@
                             base.hoverOut($(this));
                         }
                     });
+                } else if (base.options.on === 'scroll') {
+                    $(window).bind({
+                        // On scroll
+                        scroll: function(e){
+                            base.hoverIn($(base.options.elArray.join()), e.type);
+                        }
+                    });
+                } else {
+                    console.log("OKType Error: The 'on' option must be set to 'hover' or 'scroll'.");
                 }
-                // Animte in perpetuity
-            } else {
-                base.hoverIn(base.$el);
             }
         };
 
@@ -82,48 +101,42 @@
         // in an object to return our element to its old state and add a class.
         //
         // Takes one argument, the element we are hovering on.
-        base.hoverIn = function(el){
+        base.hoverIn = function(el, event){
             // Cache jQuery object
-            self = $(el);
-
-            // Our object for saving the element's history
-            restore = {
-                fontFamily : self.css('font-family'),
-                fontWeight : self.css('font-weight'),
-                fontSize : self.css('font-size'),
-                color : self.css('color'),
-                textTransform : self.css('text-transform'),
-                textDecoration : self.css('text-decoration'),
-                letterSpacing : self.css('letter-spacing')
-            }
+            $self = $(el);
             
             // Adds a convienence class
-            if (base.options.hover) self.addClass('oktype-hovering');
-            
-            // The timeout
-            timer = window.setInterval(function(){
+            if (base.options.hover) $self.addClass('oktype-hovering');
 
-                // changes the CSS with fallbacks to the current CSS
-                self.css({
-                    'font-family': base.randomType(),
-                    'font-weight': base.options.weight ? base.weight() : self.css('font-weight'),
-                    'font-size': (base.options.maxSize && base.options.minSize) ? base.randomSize() : self.css('font-size'),
-                    'color': base.options.randomColor ? base.randomColor() : self.css('color'),
-                    'text-transform': base.options.toggleCase ? base.toggleCase() : self.css('text-transform'),
-                    'text-decoration': base.options.randomDecoration ? base.randomDecoration() : self.css('text-decoration'),
-                    'letter-spacing': (base.options.minSpace && base.options.maxSpace) ? base.randomSpacing() : self.css('letter-spacing')
-                });
-                // the rate of the timeout
-            }, base.options.interval);
-            
+            if (base.options.on != "scroll" && event != "scroll") {
+
+                // Our object for saving the element's history
+                restore = {
+                    fontFamily : $self.css('font-family'),
+                    fontWeight : $self.css('font-weight'),
+                    fontSize : $self.css('font-size'),
+                    color : $self.css('color'),
+                    textTransform : $self.css('text-transform'),
+                    textDecoration : $self.css('text-decoration'),
+                    letterSpacing : $self.css('letter-spacing')
+                }
+                
+                // The timeout
+                timer = window.setInterval(function(){
+                    base.transform($self);
+                }, base.options.interval);
+            } else {
+                timer = window.setTimeout(base.transform($self), 1000);
+            }
         };
-
 
         // Removes our timeout and CSS class. Will return the element to its prior state unless explicitly stated by the user.
         base.hoverOut = function(){
 
-            // Destroy the timeout
-            clearTimeout(timer);
+            if (base.options.on != "scroll") {
+                // Destroy the timeout
+                clearTimeout(timer);
+            }
 
             // Remove the class
             if (base.options.hover) self.removeClass('oktype-hovering');
@@ -131,7 +144,7 @@
             // Sould we restore CSS?
             if (base.options.restore){
                 // Restore the element's prior CSS
-                self.css({
+                $self.css({
                     'font-family': restore.fontFamily,
                     'font-weight': restore.fontWeight,
                     'font-size': restore.fontSize,
@@ -140,7 +153,6 @@
                     'text-decoration': restore.textDecoration,
                     'letter-spacing': restore.letterSpacing
                 });
-
             }
         };
 
@@ -153,6 +165,22 @@
             } else {
                 console.log("OKType Error: You must set the option for weight to be either 'toggle' or 'random'.");
             }
+        }
+
+        base.ease = function(time){
+            return (1 - Math.cos(time * Math.PI)) / 2;
+        }
+
+        base.transform = function(elm){
+            $(elm).css({
+                'font-family': base.randomType(),
+                'font-weight': base.options.weight ? base.weight() : $(elm).css('font-weight'),
+                'font-size': (base.options.maxSize && base.options.minSize) ? base.randomSize() : $(elm).css('font-size'),
+                'color': base.options.randomColor ? base.randomColor() : $(elm).css('color'),
+                'text-transform': base.options.toggleCase ? base.toggleCase() : $(elm).css('text-transform'),
+                'text-decoration': base.options.randomDecoration ? base.randomDecoration() : $(elm).css('text-decoration'),
+                'letter-spacing': (base.options.minSpace && base.options.maxSpace) ? base.randomSpacing() : $(elm).css('letter-spacing')
+            });            
         }
 
         // Returns the CSS value for the 'font-weight' property, randomly toggling between 'bold' and 'normal'.
@@ -210,7 +238,8 @@
         maxSpace: null, // The minimum desired letter-spacing for type (an int)
         minSpace: null, // The maximum desired letter-spacing for type (an int)
         restore: true, // Restore the element to its prior state on hoverOut (a boolean)
-        hover: true // Execute animation on hover? (a boolean)
+        on: 'hover', // Execute animation on hover? (a boolean)
+        elArray: ['p', 'h1', 'h2', 'h3', 'h4', 'span', 'td', 'a'] // Elements ETV.js will effect if 'on' is set to 'scroll'
     };
 
     $.fn.oktype = function (options) {
